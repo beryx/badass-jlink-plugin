ba850671dc14058b3c9c6013e4c8d1e2da926f3a
airsquared <36649395+airsquared@users.noreply.github.com>
2023-09-24 13:50:18 -0700

    Fix docs publish

--- publish.sh
+++ publish.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -ev
 
+GRADLE_COMMAND='./gradlew --info gitPublishPush --rerun-tasks'
+
 if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
   if [ "$(git ls-remote origin gh-pages)" == "" ]; then
     echo Start gitPublishPush with ghPageType=init
-    ./gradlew --no-daemon -i -s gitPublishPush --rerun-tasks -PghPageType=init
+    $GRADLE_COMMAND -PghPageType=init
     echo Finished gitPublishPush with ghPageType=init
   fi
   echo Start gitPublishPush with ghPageType=latest
-  ./gradlew --no-daemon -i -s gitPublishPush --rerun-tasks -PghPageType=latest
+  $GRADLE_COMMAND -PghPageType=latest
   echo Finished gitPublishPush with ghPageType=version
 
   echo Start gitPublishPush with ghPageType=version
-  ./gradlew --no-daemon -i -s gitPublishPush --rerun-tasks -PghPageType=version
+  $GRADLE_COMMAND -PghPageType=version
   echo Finished gitPublishPush with ghPageType=version
 
   echo Start updating releases.md
-  ./gradlew --no-daemon -i -s update-release-list gitPublishPush --rerun-tasks -PghPageType=list
+  $GRADLE_COMMAND -PghPageType=list
   echo Finished updating releases.md
 fi
