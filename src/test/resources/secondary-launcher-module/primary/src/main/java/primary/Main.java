861f2a0d41ed2b9ec4c9909a274836100c1395b1
Axel Howind <axh@dua3.com>
2026-03-15 06:50:06 +0100

    add testcase for #290

---
+++ src/test/resources/secondary-launcher-module/primary/src/main/java/primary/Main.java
@@ -0,0 +1,6 @@
+package primary;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Primary");
+    }
+}
---
+++ src/test/resources/secondary-launcher-module/secondary/src/main/java/secondary/Main.java
@@ -0,0 +1,6 @@
+package secondary;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Secondary");
+    }
+}
