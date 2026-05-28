0bc9aa86c158d9dc76657e66d0be7df7a1e27810
Axel Howind <axh@dua3.com>
2026-05-28 08:21:21 +0200

    add test case for #351

--- src/test/groovy/org/beryx/jlink/JPackageJvmArgsSpec.groovy
+++ src/test/groovy/org/beryx/jlink/JPackageJvmArgsSpec.groovy
@@ -80,4 +80,51 @@ class JPackageJvmArgsSpec extends AbstractJlinkPluginTest {
         assert cfgFile.exists()
         assert cfgFile.text.contains('java-options=-Dmy.secondary.arg=secondary-value')
     }
+
+    def "jpackage should prioritize launcher jvmArgs over applicationDefaultJvmArgs"() {
+        given:
+        File buildFile = setUpBuild('multi-launch')
+        buildFile << """
+            application {
+                applicationDefaultJvmArgs = ["-Dapp.default.arg=app-value"]
+            }
+            jlink {
+                launcher {
+                    name = 'hello'
+                    jvmArgs = ["-Dlauncher.arg=launcher-value"]
+                }
+            }
+            jpackage {
+            }
+        """.stripIndent()
+
+        when:
+        BuildResult result = runGradleWithLockRetry(GradleRunner.create()
+                .withDebug(false)
+                .withProjectDir(testProjectDir.toFile())
+                .withPluginClasspath()
+                .withArguments('jpackageImage', '-is'))
+
+        then:
+        result.task(":jpackageImage").outcome == TaskOutcome.SUCCESS
+
+        def os = OperatingSystem.current
+        def imageName = 'hello'
+        def cfgPath
+        if (os.windows) {
+            cfgPath = "build/jpackage/$imageName/app/${imageName}.cfg"
+        } else if (os.macOs) {
+            cfgPath = "build/jpackage/${imageName}.app/Contents/app/${imageName}.cfg"
+        } else {
+            cfgPath = "build/jpackage/$imageName/lib/app/${imageName}.cfg"
+        }
+
+        def cfgFile = new File(testProjectDir.toFile(), cfgPath)
+        assert cfgFile.exists()
+        // According to the docs: "defaultValue: the jvmArgs value configured in the launcher block,
+        // or the arguments configured in the applicationDefaultJvmArgs property of the application extension."
+        // This suggests launcher jvmArgs should be used if present.
+        assert cfgFile.text.contains('java-options=-Dlauncher.arg=launcher-value')
+        assert !cfgFile.text.contains('java-options=-Dapp.default.arg=app-value')
+    }
 }
